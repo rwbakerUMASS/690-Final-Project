a70bff8654c5aa73d86113a123af54715fffe418
rwbakerUMASS <rwbaker@umass.edu>
2024-04-26 00:15:36 -0400

    plot util

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -1,15 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = np.load('./data/logs/evaluations.npz')
-print(f.files)
-x = f['timesteps']
-N = 10
-y_mean = np.convolve(np.mean(f['results'],axis=1),np.ones(N)/N,'same')
-y_min = np.convolve(np.min(f['results'],axis=1),np.ones(N)/N,'same')
-y_max = np.convolve(np.max(f['results'],axis=1),np.ones(N)/N,'same')
-plt.plot(x,y_mean)
-# plt.plot(x,y_min)
-plt.plot(x,y_max)
+
+N = 4
+
+base = np.load('data/logs/biped/basic/eval_on_hardcore/evaluations.npz')
+hardcore = np.load('data/logs/biped/hardcore_transfer/evaluations.npz')
+combined = np.concatenate([base['results'],hardcore['results']])
+x = np.concatenate([base['timesteps'],np.max(base['timesteps'])+hardcore['timesteps']])/6400
+y_mean = np.convolve(np.mean(combined,axis=1),np.ones(N)/N,'same')
+y_std = np.convolve(np.std(combined,axis=1),np.ones(N)/N,'same')
+plt.plot(x,y_mean,label='Direct Transfer')
+plt.fill_between(x,y_mean-y_std,y_mean+y_std, alpha=0.2)
+
+
+hardcore = np.load('data/logs/biped/hardcore/evaluations.npz')
+combined = hardcore['results']
+x = hardcore['timesteps']/6400
+y_mean = np.convolve(np.mean(combined,axis=1),np.ones(N)/N,'same')
+y_std = np.convolve(np.std(combined,axis=1),np.ones(N)/N,'same')
+plt.plot(x,y_mean,label='Hardcore Only')
+plt.fill_between(x,y_mean-y_std,y_mean+y_std, alpha=0.2)
+
+
+hardcore = np.load('data/logs/biped/trex/eval_on_hardcore/evaluations.npz')
+combined = np.concatenate([base['results'],hardcore['results']])
+x = np.concatenate([base['timesteps'],np.max(base['timesteps'])+hardcore['timesteps']])/6400
+y_mean = np.convolve(np.mean(combined,axis=1),np.ones(N)/N,'same')
+y_std = np.convolve(np.std(combined,axis=1),np.ones(N)/N,'same')
+plt.plot(x,y_mean,label='TREX Transfer')
+plt.fill_between(x,y_mean-y_std,y_mean+y_std, alpha=0.2)
+
+plt.legend()
 plt.show()
 pass
