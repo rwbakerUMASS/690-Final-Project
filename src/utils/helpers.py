214e2cb4437185e82bf0d4e5809fb9b9b250ffe9
rwbakerUMASS <rwbaker@umass.edu>
2024-04-28 17:58:39 -0400

    add random trex training

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -35,22 +35,26 @@ def Create_Random_Trajectories(num_trajectories: int, steps: int):
     
     action_size = env.action_space.shape[0]
     trajectories = []
+    cum_rews = []
     while num_trajectories > 0:
         time_steps = steps
         env.reset()
         done = False
         trajectory = []
+        cum_reward = 0
         while time_steps > 0 and not done:
             #done = False
             env.render()
             action = np.random.uniform(-1.0, 1.0, size=action_size)
             next_state, reward, done, truncated, info = env.step(action)
+            cum_reward += reward
             time_steps -= 1
             trajectory.append(np.array(next_state))
         trajectory = np.array(trajectory)
         num_trajectories -= 1
         trajectories.append(trajectory)
-    return trajectories
+        cum_rews.append(cum_reward)
+    return trajectories, cum_rews
 
                 
 rand_trajs = Create_Random_Trajectories(2, 100)
